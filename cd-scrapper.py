a6d6d701f86e89a887a4b99f877b5fec9946fbbf
Isaac Rodriguez <isaac_rodriguez@live.com>
2017-09-23 17:47:11 -0400

    Started implementing CDArticle

--- cd-scrapper.py
+++ cd-scrapper.py
@@ -1,10 +1,65 @@
+import bs4
 import newspaper
+import encodings.utf_16
 
-cd_url = "http://civilizeddevelopment.typepad.com"
 
-cd_paper = newspaper.build(cd_url, memoize_articles=False)
-cd_articles = [article for article in cd_paper.articles if article.url.startswith(cd_url)]
-for article in sorted(cd_articles, key=lambda a: a.url):
-    print(article.url)
+def main():
+    url ="http://civilizeddevelopment.typepad.com/civilized-development/2013/08/dragging-labels-in-2014.html"
+    article = CDArticle(url)
+    print("Title: " + article.title)
+    print("Author: " + article.author)
+    print("Categories: " + ", ".join(article.categories))
+    #print("Content", article.content)
+    article.content
 
-print('Found {c} articles'.format(c=len(cd_articles)))
+
+
+
+class CDArticle(object):
+    """
+    """
+    def __init__(self, article_url):
+        self._url = article_url
+        self._config = newspaper.Config()
+        self._config.memoize_articles = False
+        self._config.keep_article_html = True
+        self._np_article = newspaper.Article(self._url, self._config)
+        self._np_article.download()
+        self._np_article.parse()
+        self._soup = bs4.BeautifulSoup(self._np_article.html, 'html.parser')
+        self._content_soup = self._soup.find('div', class_='entry-body')
+
+
+
+    @property
+    def title(self):
+        return self._np_article.title
+
+
+    @property
+    def author(self):
+        return "Isaac Rodriguez"
+
+
+    @property
+    def categories(self):
+        footer = self._soup.find('span', class_="post-footers")
+        return [category.contents[0] for category in footer.contents if category.name == 'a']
+
+
+    @property
+    def content(self):
+        print(self._content_soup.prettify(formatter='html'))
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
